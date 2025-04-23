3e09bbb56836f067934a3215a880b604ca5a3893
bahaapix <bahaa13pix@gmail.com>
2025-04-23 08:22:38 +0100

    Delete src/shell directory

--- src/shell/arch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir ../gits && cd ../gits
-git clone https://github.com/bahaa13v/arch.git
-cd arch && chmod +x arch.sh
-bash arch.sh
--- src/shell/debian.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir ../gits && cd ../gits
-git clone https://github.com/bahaa13v/debian.git
-cd debian && chmod +x debian.sh
-bash debian.sh
